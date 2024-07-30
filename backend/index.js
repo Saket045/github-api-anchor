53a2fb372fc4e0728281a94ef727368c556ad3c7
Saket045 <saketnigam7@gmail.com>
2024-07-30 09:52:54 +0530

    last

--- backend/index.js
+++ backend/index.js
@@ -32,7 +32,7 @@ app.get("*",(req,res)=>{
 })
 
 console.log("dirname",__dirname);
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on port ${process.env.PORT}`);
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
     connectMongoDb();
 })
