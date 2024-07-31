2f428e4bc8d39b4abba6f8cb9ebd286d7c6a12bd
Saket045 <saketnigam7@gmail.com>
2024-07-31 09:10:46 +0530

    "ignore commit"

--- backend/Passport/github.auth.js
+++ backend/Passport/github.auth.js
@@ -3,8 +3,7 @@ import { Strategy as GitHubStrategy } from 'passport-github2';
 import dotenv from 'dotenv';
 import userModel from '../models/user.model.js'
 import User from '../models/user.model.js';
-const GITHUB_CLIENT_ID="Ov23liJxeZYkxamlXjlm";
-const GITHUB_CLIENT_SECRET="64c1378d906de5d32470981718666f8895eac42f"
+
 
 dotenv.config();
 passport.serializeUser(function(user, done) {
@@ -20,8 +19,8 @@ passport.serializeUser(function(user, done) {
   //   credentials (in this case, an accessToken, refreshToken, and GitHub
   //   profile), and invoke a callback with a user object.
   passport.use(new GitHubStrategy({
-      clientID: GITHUB_CLIENT_ID,
-      clientSecret:  GITHUB_CLIENT_SECRET,
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
       callbackURL: "/api/auth/github/callback"
     },
     async function(accessToken, refreshToken, profile, done) {
