735b9349a16cc11db29ffd0022b9ff1d613e182a
Saket045 <saketnigam7@gmail.com>
2024-08-02 10:31:54 +0530

    connectMongoDb

--- backend/db/connectMongoDb.js
+++ backend/db/connectMongoDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default async function connectMongoDb(){
     try{
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb://localhost:27017/github-db");
     console.log("MongoDB Connected");
     }
     catch(error){
